63bfdf708fb91badc4a2d6d93049a467b0f3f9de
Hiroki Oiwa <hiroki@hackleberry.me>
2014-07-14 16:28:43 +0900

    Update helloworld.swift

--- helloworld.swift
+++ helloworld.swift
@@ -2,5 +2,18 @@
 
 import Cocoa
 
+// String
 let greeting: String = "Hello world"
 let kirakira: String = "✨"
+let it: String = "Go"
+
+let hoge: String = "\(it) \(greeting + kirakira)"
+
+// Int, Float, Double
+let num: Int = 100
+let pie1: Float = 3.14
+let pie2: Double = 3.14
+
+// Array, Dictionary
+let animalList: String[] = ["rion", "cat", "dog"]
+
